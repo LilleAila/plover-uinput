1da02e7a3e7d4863d968c5f47dfba6c026c37e4d
LilleAila <olai.solsvik@gmail.com>
2024-04-11 13:36:39 +0200

    Completed (?) keymap symbols

--- plover_uinput/symbols.py
+++ plover_uinput/symbols.py
@@ -7,6 +7,8 @@ from xkbcommon import xkb
 def generate_symbols(layout="no"):
     ctx = xkb.Context()
     keymap = ctx.keymap_new_from_names(layout=layout)
+    # The keymaps have to be "translated" to a US layout keyboard for evdev
+    keymap_us = ctx.keymap_new_from_names(layout="us")
 
     # Modifier names from xkb, converted to strings
     level_mapping = {
@@ -26,19 +28,19 @@ def generate_symbols(layout="no"):
                 continue
 
             # === Base key symbol ===
-            base_key_syms = keymap.key_get_syms_by_level(key, 1, 0)
+            base_key_syms = keymap_us.key_get_syms_by_level(key, 1, 0)
             if len(base_key_syms) == 0:  # There are no symbols for this key
                 continue
             base_key = xkb.keysym_to_string(base_key_syms[0])
             if base_key is None:
                 continue
-            symbols[base_key] = (base_key, "")
+            # symbols[base_key] = (base_key, "")
             # === Base key symbol ===
 
             # === Key variations ===
-            if levels < 2:  # There are no variations (Check maybe not needed)
+            if levels < 1:  # There are no variations (Check maybe not needed)
                 continue
-            for level in range(1, levels + 1):  # Ignoring the first (base) one
+            for level in range(0, levels + 1):  # Ignoring the first (base) one
                 level_key_syms = keymap.key_get_syms_by_level(key, 1, level)
                 if len(level_key_syms) == 0:
                     continue
@@ -46,10 +48,14 @@ def generate_symbols(layout="no"):
                 if level_key is None:
                     continue
                 modifiers = level_mapping.get(level, "")
-                symbols[level_key] = (base_key, modifiers)
+                if not level_key in symbols:
+                    symbols[level_key] = (base_key, modifiers)
             # === Key variations ===
         except xkb.XKBInvalidKeycode:
             # Iter *should* return only valid, but still returns some invalid...
             pass
 
     return symbols
+
+
+syms = generate_symbols("no")
